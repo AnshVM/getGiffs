fc539c71aaedc55664d37169059d0a48f5389b19
AnshVM <anshmalik2002@gmail.com>
2021-02-09 19:39:16 +0530

    Added search joke

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 const express = require("express");
 const app = express();
 const https = require('https');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 
 const url = "https://v2.jokeapi.dev/joke/Any?type=single";
 const apikey = "cGusdYvbs9cKWg5HWs6MFFp5dHgwtVtT";
 
 app.get("/",(req,res)=>{
-    https.get(url,(response)=>{
+    res.sendFile(__dirname+"/index.html");
+    //https.get(url,(response)=>{
+    //    console.log(response.statusCode);
+    //    response.on("data",(d)=>{
+    //        const jokeData = JSON.parse(d);
+    //        res.send(jokeData.joke);
+    //    });
+    //});
+});
+
+app.post("/",(req,res)=>{
+    const queryURL = url + "&contains=" + req.body.query;
+    console.log(queryURL);
+    https.get(queryURL,(response)=>{
         console.log(response.statusCode);
         response.on("data",(d)=>{
             const jokeData = JSON.parse(d);
-            res.send(jokeData.joke);
+            console.log(jokeData);
+
+            if(jokeData.type == 'single'){
+                res.send(jokeData.joke);
+            }
+            else if(jokeData.type == "twopart"){
+                res.send(jokeData.setup);
+                res.send(jokeData.delivery);               
+            }
+
+            else if(jokeData.error == true){
+                res.send(jokeData.message);
+            }
         });
     });
+
 });
 
 app.listen(3000,()=>{
