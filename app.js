03dff9ee5f6d351b70fc230319a54c43e006e318
AnshVM <anshmalik2002@gmail.com>
2021-02-10 10:26:21 +0530

    Search and categories completed

--- app.js
+++ app.js
@@ -2,26 +2,38 @@ const express = require("express");
 const app = express();
 const https = require('https');
 const bodyParser = require("body-parser");
+const { on } = require("events");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
 
-const url = "https://v2.jokeapi.dev/joke/Any?type=single";
-const apikey = "cGusdYvbs9cKWg5HWs6MFFp5dHgwtVtT";
+var url = "https://v2.jokeapi.dev/joke";
+console.log("url init: "+url)
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
-    //https.get(url,(response)=>{
-    //    console.log(response.statusCode);
-    //    response.on("data",(d)=>{
-    //        const jokeData = JSON.parse(d);
-    //        res.send(jokeData.joke);
-    //    });
-    //});
 });
 
 app.post("/",(req,res)=>{
-    const queryURL = url + "&contains=" + req.body.query;
-    console.log(queryURL);
-    https.get(queryURL,(response)=>{
+    url = "https://v2.jokeapi.dev/joke";
+    const categoryData = (req.body);
+    var categoryURL = "/";
+    for(x in categoryData){
+        if(categoryData[x]=='on'){
+            categoryURL += x + ",";
+        }
+    }
+    categoryURL = categoryURL.slice(0,categoryURL.length-1);
+    if(categoryURL==''){
+        url = url+"/Any?contains="+req.body.query;
+    }
+    else{
+        url = url + categoryURL;
+        if(req.body.query!=''){
+            url = url + "?contains=" + req.body.query;
+        }
+    }
+    console.log(url);
+    https.get(url,(response)=>{
         console.log(response.statusCode);
         response.on("data",(d)=>{
             const jokeData = JSON.parse(d);
@@ -31,8 +43,7 @@ app.post("/",(req,res)=>{
                 res.send(jokeData.joke);
             }
             else if(jokeData.type == "twopart"){
-                res.send(jokeData.setup);
-                res.send(jokeData.delivery);               
+                res.send(jokeData.setup+" "+jokeData.delivery);             
             }
 
             else if(jokeData.error == true){
